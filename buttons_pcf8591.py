697054733359c5858ba4d0c7d25a34ea44ae3dc1
alexandru360 <alexandru360@gmail.com>
2023-03-31 22:33:58 +0300

    fix

--- buttons_pcf8591.py
+++ buttons_pcf8591.py
@@ -1,13 +1,26 @@
-from buttons_pcf8591 import PCF8591
-import time
+# buttons_pcf8591.py
 
-if __name__ == "__main__":
-    pcf8591 = PCF8591(bus_number=1)  # Use I2C bus 1
+from board import SCL, SDA
+import busio
 
-    ain0_value = pcf8591.read_AIN0()
-    ain1_value = pcf8591.read_AIN1()
-    ain2_value = pcf8591.read_AIN2()
+class PCF8591:
+    def __init__(self, address=0x48, bus_number=1):
+        self.address = address
+        self.bus = busio.I2C(SCL, SDA)
 
-    print(f"AIN0: {ain0_value}, AIN1: {ain1_value}, AIN2: {ain2_value}")
+    def read(self, channel):
+        assert 0 <= channel <= 3, "Channel must be between 0 and 3 (inclusive)"
+        self.bus.writeto(self.address, bytes([channel]))
+        self.bus.readfrom_into(self.address, 1)  # Discard the first byte (dummy read)
+        result = bytearray(1)
+        self.bus.readfrom_into(self.address, result)
+        return result[0]
 
-    time.sleep(1)
+    def read_AIN0(self):
+        return self.read(0)
+
+    def read_AIN1(self):
+        return self.read(1)
+
+    def read_AIN2(self):
+        return self.read(2)
