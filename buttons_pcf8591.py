adec6102c2d4fae8efdae7b096114d6505a8f34b
alexandru360 <alexandru360@gmail.com>
2023-03-31 21:52:39 +0300

    Not working yet but mofed all boards on i2c channel 1

--- buttons_pcf8591.py
+++ buttons_pcf8591.py
@@ -1,12 +1,12 @@
 # buttons_pcf8591.py
 
-import board
+from board import SCL, SDA
 import busio
 
 class PCF8591:
     def __init__(self, address=0x48, bus_number=1):
         self.address = address
-        self.bus = busio.I2C(getattr(board, f"SCL_{bus_number}"), getattr(board, f"SDA_{bus_number}"))
+        self.bus = busio.I2C(SCL, SDA)
 
     def read(self, channel):
         assert 0 <= channel <= 3, "Channel must be between 0 and 3 (inclusive)"
@@ -24,13 +24,3 @@ class PCF8591:
 
     def read_AIN2(self):
         return self.read(2)
-
-# Example usage
-if __name__ == "__main__":
-    pcf8591 = PCF8591()
-
-    ain0_value = pcf8591.read_AIN0()
-    ain1_value = pcf8591.read_AIN1()
-    ain2_value = pcf8591.read_AIN2()
-
-    print(f"AIN0: {ain0_value}, AIN1: {ain1_value}, AIN2: {ain2_value}")
--- main.py
+++ main.py
@@ -1,19 +1,44 @@
-from buttons_pcf8591 import PCF8591
-from servo_pca9685 import MotorController
+# main.py
+
 import time
+from buttons_pcf8591 import PCF8591
+from adafruit_pca9685 import PCA9685
 
-if __name__ == "__main__":
-    motor = MotorController(speed=100, channel=0, bus_number=1)  # Use I2C bus 1
+pca9685 = PCA9685(i2c_bus_number=1)  # Use I2C bus 1
+pcf8591 = PCF8591(bus_number=1)  # Use I2C bus 1
 
-    # motor.moveUp(increment=1)
-    motor.stop()
+SERVO_MIN = 150  # Min pulse length out of 4096
+SERVO_MAX = 600  # Max pulse length out of 4096
 
-    time.sleep(1)
+# Set channels 0 through 7 to the "neutral" signal (375 out of 4096)
+for i in range(8):
+    pca9685.channels[i].duty_cycle = 0x6FFF
 
-    pcf8591 = PCF8591(bus_number=1)  # Use I2C bus 1
+def set_servo_angle(channel, angle):
+    duty_cycle = int((angle / 180) * (SERVO_MAX - SERVO_MIN) + SERVO_MIN)
+    pca9685.channels[channel].duty_cycle = duty_cycle
 
-    ain0_value = pcf8591.read_AIN0()
-    ain1_value = pcf8591.read_AIN1()
-    ain2_value = pcf8591.read_AIN2()
+try:
+    while True:
+        ain0_value = pcf8591.read_AIN0()
+        ain1_value = pcf8591.read_AIN1()
+        ain2_value = pcf8591.read_AIN2()
 
-    print(f"AIN0: {ain0_value}, AIN1: {ain1_value}, AIN2: {ain2_value}")
+        print(f"AIN0: {ain0_value}, AIN1: {ain1_value}, AIN2: {ain2_value}")
+
+        # Map analog values to servo angles
+        servo_1_angle = int(ain0_value / 255 * 180)
+        servo_2_angle = int(ain1_value / 255 * 180)
+        servo_3_angle = int(ain2_value / 255 * 180)
+
+        # Set servo angles
+        set_servo_angle(0, servo_1_angle)
+        set_servo_angle(1, servo_2_angle)
+        set_servo_angle(2, servo_3_angle)
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("Stopping motors")
+    for i in range(8):
+        pca9685.channels[i].duty_cycle = 0x0000
