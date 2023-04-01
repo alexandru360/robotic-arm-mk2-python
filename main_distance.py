e291fb492c6465b9db22e621d43e9c43a7498fe0
alexandru360 <alexandru360@gmail.com>
2023-04-01 09:13:35 +0300

    It is working

--- main_distance.py
+++ main_distance.py
@@ -1,7 +1,11 @@
 from src.hysrf05_distance import HYSRF05
 
+# Conectăm pinul VCC la pinul 5V al Raspberry Pi, 
+# pinul GND la pinul GND al Raspberry Pi, 
+# pinul Echo la pinul GPIO 18 (BCM), 
+# pinul Trigger la pinul GPIO 23 (BCM)
 if __name__ == "__main__":
-    hysrf05 = HYSRF05(23, 24)
+    hysrf05 = HYSRF05(23, 18)
     try:
         while True:
             distance = hysrf05.measure_distance()
