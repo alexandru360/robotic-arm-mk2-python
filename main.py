3767624530b302402131b0ae72950ae57de7a663
alexandru360 <alexandru360@gmail.com>
2023-04-02 14:15:44 +0300

    something works ...

--- main.py
+++ main.py
@@ -3,21 +3,45 @@ from src.pcf8591_buttons import PCF8591
 from src.pca9685_servo import PCA9685Servo
 
 if __name__ == "__main__":
-    _channel = 3
-    _speed = 50
-
-    # Use I2C bus 1
-    motor = PCA9685Servo(speed=_speed, channel=_channel)
-
-    pcf8591 = PCF8591()
     try:
-        while True:
-            xCoord = pcf8591.read_AIN0()
-            yCoord = pcf8591.read_AIN1()
-            sw_state = pcf8591.read_AIN3()
-            string = "Button pressed" if sw_state == 0 or sw_state == 1 else "Button not pressed"
+        _channel = 1
+        _speed = 50
 
-            print(f"X: {xCoord}, Y: {yCoord}, Sw-state: {sw_state}-{string}")
+        # Use I2C bus 1
+        motor = PCA9685Servo(speed=_speed, channel=_channel)
+
+        pcf8591 = PCF8591()
+
+        while True:
+            x = pcf8591.read_AIN0()
+            y = pcf8591.read_AIN1()
+            sw = pcf8591.read_AIN3()
+            swState = "pressed" if sw == 0 or sw == 1 else "not pressed"
+
+            if sw in range(0, 2):
+                _channel = _channel + 1
+                if _channel not in range(0, 3):
+                    _channel = 1
+
+            if x in range(155, 160) and y in range(203, 206):  # up
+                motor.moveUp()
+
+            if x in range(253, 256) and y in range(203, 206):  # down
+                motor.moveUp()
+
+            # The only channel of left and right
+            if x in range(163, 167) and y in range(253, 256):  # left
+                # Use I2C bus 1
+                motor = PCA9685Servo(speed=_speed, channel=0)
+                motor.moveUp()
+
+            if x in range(163, 167) and y in range(0, 5):  # right
+                # Use I2C bus 1
+                motor = PCA9685Servo(speed=_speed, channel=0)
+                motor.moveDown()
+
+            print(
+                f"X: {x}, Y: {y}, Sw-state: {sw}-{swState}, Selected channel {_channel}")
 
             time.sleep(1)
 
