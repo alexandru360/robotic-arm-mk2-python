a0a85690d6abb14af3b3f4870e104df5ff3c36b3
Alex <alexandru360@gmail.com>
2023-04-06 13:15:09 +0300

    small modifications :)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # robotic-arm-mk2-python
 Robotic arm with servos and mini joysticks
+
+sudo i2cdetect -y -l
+
+sudo i2cdetect -y 1
---
+++ buttons_pcf8591.py
@@ -0,0 +1,26 @@
+# buttons_pcf8591.py
+
+from board import SCL, SDA
+import busio
+
+class PCF8591:
+    def __init__(self, address=0x48, bus_number=1):
+        self.address = address
+        self.bus = busio.I2C(SCL, SDA)
+
+    def read(self, channel):
+        assert 0 <= channel <= 3, "Channel must be between 0 and 3 (inclusive)"
+        self.bus.writeto(self.address, bytes([channel]))
+        self.bus.readfrom_into(self.address, bytearray(1))  # Discard the first byte (dummy read)
+        result = bytearray(1)
+        self.bus.readfrom_into(self.address, result)
+        return result[0]
+
+    def read_AIN0(self):
+        return self.read(0)
+
+    def read_AIN1(self):
+        return self.read(1)
+
+    def read_AIN2(self):
+        return self.read(2)
